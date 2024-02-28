9cb58d3142ea3ff8c36d3078d79aab872e136b9b
Masum Reja <msmreja@gmail.com>
2024-02-28 12:01:07 +0530

    Updated about picture

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import AboutImg from "../../assets/about.jpg";
+import AboutImg from "../../assets/about.jpeg";
 import CV from "../../assets/msmreja.pdf";
 import Info from "./info";
 
